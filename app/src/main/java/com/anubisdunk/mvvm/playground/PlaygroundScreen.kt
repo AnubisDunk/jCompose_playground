78efced0ef829f298478809582f0e4610870a3d9
Alexandr Baranovskyy <alex.bar.dev@gmail.com>
2024-02-10 15:37:00 +0200

    flow vs composestate

--- app/src/main/java/com/anubisdunk/mvvm/playground/PlaygroundScreen.kt
+++ app/src/main/java/com/anubisdunk/mvvm/playground/PlaygroundScreen.kt
@@ -1,10 +1,16 @@
 package com.anubisdunk.mvvm.playground
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,25 +24,40 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PlaygroundScreen(
     modifier: Modifier = Modifier,
-    viewModel: PlaygroundScreenViewModel = viewModel<PlaygroundScreenViewModel>()
+    viewModel: PlaygroundScreenViewModel = viewModel(),
+    onUserItemChanged : (String) -> Unit = {viewModel.updateUserItem(it)},
+    addItem : (String) -> Unit = {viewModel.addItem(it)},
+    item : String = viewModel.item,
+    items : List<String> = viewModel.items
 ) {
-    var count by remember{mutableStateOf(0)}
     Column(
         modifier = modifier
             .fillMaxSize()
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = count.toString(),
-            fontSize = 36.sp
-        )
-        Spacer(modifier)
-        Button(onClick = { count++ }) {
-            Text(text = "Click me")
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            OutlinedTextField(
+                modifier = Modifier.weight(1f),
+                value = item,
+                onValueChange = {onUserItemChanged(it)}
+            )
+            Button(onClick = {
+                addItem(item)
+            }) {
+                Text(text = "Click me")
+            }
+        }
+        LazyColumn {
+            items(items) { item ->
+                Text(text = item)
+            }
         }
     }
 }
--- app/src/main/java/com/anubisdunk/mvvm/playground/PlaygroundScreenViewModel.kt
+++ app/src/main/java/com/anubisdunk/mvvm/playground/PlaygroundScreenViewModel.kt
@@ -1,10 +1,25 @@
 package com.anubisdunk.mvvm.playground
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 
-class PlaygroundScreenViewModel : ViewModel(){
+class PlaygroundScreenViewModel : ViewModel() {
 
     private val _uiState = MutableStateFlow(0)
 
+    var item by mutableStateOf("")
+        private set
+    var items by mutableStateOf(listOf<String>())
+
+    fun updateUserItem(input: String){
+        item = input
+    }
+
+    fun addItem(item : String) {
+        items += item
+    }
 }
